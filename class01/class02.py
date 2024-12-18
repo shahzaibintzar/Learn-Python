c38d2aaf5ac8351607cfa4282c674eb424eb4ea4
shahzaibintzar <shahzaibintzar27346@gmail.com>
2024-12-18 12:24:15 +0500

    datatype and operator

---
+++ class01/class02.py
@@ -0,0 +1,44 @@
+                            #   dataType in python
+                         #   int, float, str, bool
+# textData= "Ali"
+# textData1= '"Ali" is my best friend'
+# textData2= ''''Ali
+# ahmed
+# subhan
+# sami
+# shahzaib
+# '''
+# textData3= f'Ali' 
+# print(textData2)
+
+num = 12345
+num1 = 3456
+# print(num)
+# num2 = 12.345
+# print(num2)
+
+# boolean
+# is_true = True
+# is_false = False
+# print(is_true)
+# print(is_false)
+
+#operator
+# result = num + num1
+# print(result)
+# result = num1 - num
+# print(result)
+
+# result = num * num1
+# print(result)
+
+# result = num / num1
+# print(result)
+
+# result = num // num1
+# print(result)
+
+# result = num / num1 + num * num - 10 / 4
+
+# print(result)
+
