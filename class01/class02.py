fc458050321017e4708d6e23bfade98c3717c83f
shahzaibintzar <shahzaibintzar27346@gmail.com>
2024-12-18 14:13:25 +0500

    operator almost complete

--- class01/class02.py
+++ class01/class02.py
@@ -11,8 +11,8 @@
 # textData3= f'Ali' 
 # print(textData2)
 
-num = 12345
-num1 = 3456
+# num = 12345
+# num1 = 3456
 # print(num)
 # num2 = 12.345
 # print(num2)
@@ -38,7 +38,78 @@ num1 = 3456
 # result = num // num1
 # print(result)
 
+# result = num ** num1
+
+# print(result)
+
+# result = num % num1
+# print(result)
+
+
 # result = num / num1 + num * num - 10 / 4
 
 # print(result)
 
+# - ()
+# - **
+# - / * // %
+# - + -
+
+
+
+# comparison operators
+# ==  > < <= >= != 
+# true US False
+
+
+
+# logical_operators
+# or case
+# result = num1 >= num2 or num1 + 1 == 2 and num1 + 15 > num2 - 5
+
+# and case
+# T T = True
+# T F = False
+# F T = False
+# F F = False
+# result = num1 >= num2 and num1 + 1 == 2
+
+
+
+# a = 10
+# a = a + 5
+# a += 5
+# a -= 10
+# a *= 10
+# a /= 10
+# a %= 10
+# a **= 10
+# a //= 10
+# print(a)
+
+
+
+
+
+
+# membership operators
+# userName = "shahzaib"
+# value = "z"
+# result = value not in userName
+# result1 = value in userName
+
+# print(result)
+
+# print(result1)
+
+
+
+
+# identity operators
+userName = "shahzaib"
+value = "shahzaib"
+result = value is userName
+result1 = value is not userName
+print(result)
+
+print(result1)
