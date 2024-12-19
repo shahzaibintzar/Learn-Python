7d0daa67805282a13a1b93287f01f8f45bf6d340
shahzaibintzar <shahzaibintzar27346@gmail.com>
2024-12-19 23:10:46 +0500

    function define

---
+++ class01/class03.py
@@ -0,0 +1,17 @@
+                                 # function definition
+# print("hy  Requirements")
+# print("1. Python 3.x or above")
+# print("2. Flask Web Framework")
+# def main ():
+#  print("3. MongoDB Database")
+#  print("4. User Registration")
+# print("5. User Login")
+# print("6. User Profile")
+# main ()
+
+def add (a,b):
+    num1 = 10
+    num2 = 20
+    result = num1 + num2 + a + b
+    print(result)
+add (100,200)
